325b56ee437ffed27bb1b9c0fe599abdf643e7bb
Wael Orraby <orraby2000@gmail.com>
2024-04-19 22:26:40 +0200

    Implementeer hoofdapplicatie met Firebase-initialisatie en navigatieroutes. De main.dart-bestand initialiseert Firebase en definieert navigatieroutes voor de Splash, Inloggen, Registreren en Homepagina's van de Playtomic-applicatie. Dit zorgt voor een gestroomlijnde gebruikerservaring en legt de basis voor verdere ontwikkeling.

--- playtomic_app/lib/main.dart
+++ playtomic_app/lib/main.dart
@@ -1,6 +1,10 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'package:flutter/material.dart';
+import 'package:playtomic_app/features/app/splash_screen/splash_screen.dart';
+import 'package:playtomic_app/features/user_auth/presentation/pages/home_page.dart';
+import 'package:playtomic_app/features/user_auth/presentation/pages/login_page.dart';
+import 'package:playtomic_app/features/user_auth/presentation/pages/sign_up_page.dart';
+import 'package:playtomic_app/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,12 +19,17 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Playtomic',
+      routes: {
+        '/': (context) => const SplashScreen(
+              child: LoginPage(),
+            ),
+        '/login': (context) => const LoginPage(),
+        '/signUp': (context) => const SignUpPage(),
+        '/home': (context) => const HomePage(),
+      },
     );
   }
 }
