692edd801faaf77afd0c436726c8a80d6efbee05
Wael-Orraby <orraby2000@gmail.com>
2024-05-16 12:28:41 +0200

    Voeg Design toe aan page

--- playtomic_app/lib/features/user_auth/presentation/pages/match_details_page.dart
+++ playtomic_app/lib/features/user_auth/presentation/pages/match_details_page.dart
@@ -90,131 +90,133 @@ class _MatchDetailsPageState extends State<MatchDetailsPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              'TEAMS:',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 22,
-                color: Colors.blue, // Verander de tekstkleur naar blauw
-              ),
-            ),
-            const SizedBox(height: 10),
-            const Text(
-              'Team 1 Spelers:',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 18,
-                color: Colors.black, // Verander de tekstkleur naar zwart
-              ),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: team1PlayersData
-                  .map(
-                    (player) => Text(
-                      player ?? '',
-                      style: const TextStyle(fontSize: 16),
-                    ),
-                  )
-                  .toList(),
-            ),
-            const SizedBox(height: 10),
-            const Text(
-              'Team 2 Spelers:',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 18,
-                color: Colors.black, // Verander de tekstkleur naar zwart
-              ),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: team2PlayersData
-                  .map(
-                    (player) => Text(
-                      player ?? '',
-                      style: const TextStyle(fontSize: 16),
-                    ),
-                  )
-                  .toList(),
-            ),
-            const SizedBox(height: 20),
-            const Text(
-              'SETS',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 22,
-                color: Colors.blue, // Verander de tekstkleur naar blauw
-              ),
-            ),
-            const SizedBox(height: 10),
-            if (totaalSets != null)
-              Column(
-                children: totaalSets!.asMap().entries.map((entry) {
-                  int i = entry.key;
-                  Sets? set = entry.value;
-                  return Padding(
-                    padding: const EdgeInsets.all(5.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                            color: Colors.blue), // Voeg een blauwe rand toe
-                        borderRadius:
-                            BorderRadius.circular(10), // Rond de hoeken af
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Set ${i + 1}:",
-                              style: const TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                                color: Colors
-                                    .blue, // Verander de tekstkleur naar blauw
-                              ),
-                            ),
-                            const SizedBox(height: 8),
-                            Row(
-                              children: [
-                                Expanded(
-                                  child: Text(
-                                    'Team 1: ${set?.team1}',
-                                    style: const TextStyle(fontSize: 16),
-                                  ),
-                                ),
-                                Expanded(
-                                  child: Text(
-                                    'Team 2: ${set?.team2}',
-                                    style: const TextStyle(fontSize: 16),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  );
-                }).toList(),
-              ),
-            if (status == "completed")
-              Center(
-                child: Text(
-                  'Winner is: $winner!',
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 30,
-                    color: Colors.green, // Verander de tekstkleur naar groen
-                  ),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'TEAMS:',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22,
+                  color: Colors.blue, // Verander de tekstkleur naar blauw
                 ),
               ),
-          ],
+              const SizedBox(height: 10),
+              const Text(
+                'Team 1 Spelers:',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                  color: Colors.black, // Verander de tekstkleur naar zwart
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: team1PlayersData
+                    .map(
+                      (player) => Text(
+                        player ?? '',
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                    )
+                    .toList(),
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                'Team 2 Spelers:',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                  color: Colors.black, // Verander de tekstkleur naar zwart
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: team2PlayersData
+                    .map(
+                      (player) => Text(
+                        player ?? '',
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                    )
+                    .toList(),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'SETS',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22,
+                  color: Colors.blue, // Verander de tekstkleur naar blauw
+                ),
+              ),
+              const SizedBox(height: 10),
+              if (totaalSets != null)
+                Column(
+                  children: totaalSets!.asMap().entries.map((entry) {
+                    int i = entry.key;
+                    Sets? set = entry.value;
+                    return Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                              color: Colors.blue), // Voeg een blauwe rand toe
+                          borderRadius:
+                              BorderRadius.circular(10), // Rond de hoeken af
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Set ${i + 1}:",
+                                style: const TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors
+                                      .blue, // Verander de tekstkleur naar blauw
+                                ),
+                              ),
+                              const SizedBox(height: 8),
+                              Row(
+                                children: [
+                                  Expanded(
+                                    child: Text(
+                                      'Team 1: ${set?.team1}',
+                                      style: const TextStyle(fontSize: 16),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: Text(
+                                      'Team 2: ${set?.team2}',
+                                      style: const TextStyle(fontSize: 16),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                ),
+              if (status == "completed")
+                Center(
+                  child: Text(
+                    'Winner is: $winner!',
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+                      color: Colors.green, // Verander de tekstkleur naar groen
+                    ),
+                  ),
+                ),
+            ],
+          ),
         ),
       ),
     );
